1e522b0d56630417051156cba6baf760f4e2d353
Richard <richardlambuwun10@gmail.com>
2026-08-28 16:19:48 +0800

    Fix layout syntax and clean up scratch files

--- scratch/reset_db.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function run() {
-  await prisma.category.deleteMany();
-  console.log('Categories reset.');
-  await prisma.$disconnect();
-}
-run();
--- scratch/test_trx.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function clean() {
-  await prisma.transaction.deleteMany({ where: { id: { startsWith: 'MEJA-1-MT15TL1A' } } });
-  console.log('Cleaned test transaction.');
-  await prisma.$disconnect();
-}
-clean();
